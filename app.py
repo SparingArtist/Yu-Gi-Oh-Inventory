a4042b953ac3750ed99b82ab13ac0a158f8d0196
Hashbrown <mehashimakram@gmail.com>
2025-03-21 17:24:58 -0400

    second commit

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def index():
     return render_template("index.html", card=card_data, error=error)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
